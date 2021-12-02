677cd040260e408ebfe02d0a204be451ef22825f
destiall <destialpvp@gmail.com>
2021-12-02 18:23:53 +0800

    move test folder

--- test/!run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Setting JAVA_HOME
-set JAVA_HOME=C:\Program Files\Java\jdk-16.0.1
-echo setting PATH
-set PATH=C:\Program Files\Java\jdk-16.0.1\bin;%PATH%
-java -Xms128M -Xmx4048M -jar paper.jar -nogui
-PAUSE
